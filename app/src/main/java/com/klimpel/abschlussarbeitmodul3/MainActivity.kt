8fa8cb3d870a468440577b0d06514dbfc55cfef0
xatori <xatori@gmx.de>
2023-10-16 14:37:05 +0200

    Projekt erstellt

---
+++ app/src/main/java/com/klimpel/abschlussarbeitmodul3/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.klimpel.abschlussarbeitmodul3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.klimpel.abschlussarbeitmodul3.ui.theme.AbschlussarbeitModul3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AbschlussarbeitModul3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
