8e96fc831d23c269b540cae13f63b33f0a84a1b7
xatori <xatori@gmx.de>
2023-10-17 09:11:21 +0200

    Interface für die Pokemon API erstellt und mit Kommentaren versehen.

--- app/src/main/java/com/klimpel/abschlussarbeitmodul3/data/remote/PokeApi.kt
+++ app/src/main/java/com/klimpel/abschlussarbeitmodul3/data/remote/PokeApi.kt
@@ -1,4 +1,36 @@
 package com.klimpel.abschlussarbeitmodul3.data.remote
 
+import com.asunakangura.pokemonbattle.data.remote.responses.Pokemon
+import com.asunakangura.pokemonbattle.data.remote.responses.PokemonList
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+/**
+ * Eine Schnittstelle für die PokeApi, die verschiedene Endpunkte zum Abrufen von Pokémon-Daten bereitstellt.
+ */
 interface PokeApi {
+    /**
+     * Ruft eine Liste von Pokémon ab, basierend auf einer begrenzten Anzahl und einem Versatz.
+     *
+     * @param limit Die maximale Anzahl von Pokémon in der Liste.
+     * @param offset Der Versatz, um das Abrufen der Pokémon-Liste zu starten.
+     * @return Eine PokemonListe mit den abgerufenen Pokémon.
+     */
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): PokemonList
+
+    /**
+     * Ruft Informationen zu einem bestimmten Pokémon basierend auf seinem Namen ab.
+     *
+     * @param name Der Name des Pokémon, von dem Informationen abgerufen werden sollen.
+     * @return Ein Pokémon-Objekt mit den abgerufenen Informationen.
+     */
+    @GET("pokemon/{name}")
+    suspend fun getPokemonInfo(
+        @Path("name") name: String
+    ): Pokemon
 }
